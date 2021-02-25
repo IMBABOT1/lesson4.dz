f3ba7b55e43170567a2e27d3e2aa3ab25d3cbcf6
IMBABOT1 <zlovred12@gmail.com>
2021-02-25 19:55:13 +0300

    Initial commit

---
+++ src/test/java/ru/geekrains/spring1/lesson4/dz/lesson4/dz/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekrains.spring1.lesson4.dz.lesson4.dz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
